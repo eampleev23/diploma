064c7fff917889b60d79133baeea79ebbd5c366e
Евгений Амплеев <ampleeve@MacBook-Pro-Evgenij.local>
2024-01-29 07:22:13 +0300

    add json header checking

--- internal/handlers/register.go
+++ internal/handlers/register.go
@@ -1,13 +1,17 @@
 package handlers
 
 import (
-	"log"
 	"net/http"
+	"strings"
 )
 
 // Register регистрирует нового пользователя
 func (h *Handlers) Register(w http.ResponseWriter, r *http.Request) {
 	// Сначала получим контент тайп и отдадим соответствующий ответ если это не JSON
 	contentType := r.Header.Get("Content-Type")
-	log.Println("contentType=", contentType)
+	supportsJson := strings.Contains(contentType, "application/json")
+	if !supportsJson {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 }
