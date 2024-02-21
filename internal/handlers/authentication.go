a8e443532f62fb0315e5d88b77e8d8efb408bca2
Евгений Амплеев <ampleeve@192.168.1.4>
2024-02-21 22:46:04 +0300

    init commit

---
+++ internal/handlers/authentication.go
@@ -0,0 +1,10 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+// Authentication авторизует зарегистрированного пользователя
+func (h *Handlers) Authentication(w http.ResponseWriter, r *http.Request) {
+
+}
