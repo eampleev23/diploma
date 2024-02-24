907a9451bbf17c9738c9ebb3e48495690fa5d3ae
Евгений Амплеев <ampleeve@192.168.1.4>
2024-02-24 19:13:42 +0300

    add 400, 401, 500

--- internal/handlers/upload_order.go
+++ internal/handlers/upload_order.go
@@ -1,10 +1,33 @@
 package handlers
 
 import (
+	"fmt"
+	"go.uber.org/zap"
 	"net/http"
+	"strings"
 )
 
 // UploadOrder добавляет новый заказ в систему (заявка на получение баллов лояльности)
 func (h *Handlers) UploadOrder(w http.ResponseWriter, r *http.Request) {
-
+	// Проверяем формат запроса.
+	contentType := r.Header.Get("Content-Type")
+	textPlain := strings.Contains(contentType, "text/plain")
+	if !textPlain {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	// Ппроверяем, не авторизован ли пользователь, отправивший запрос.
+	userID, isAuth, err := h.GetUserID(r)
+	if err != nil {
+		h.l.ZL.Error("GetUserID fail")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if !isAuth {
+		h.l.ZL.Debug("Unaithorized user")
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	h.l.ZL.Debug("isAuth", zap.Bool("auth", isAuth))
+	fmt.Println(userID)
 }
