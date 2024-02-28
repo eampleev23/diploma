bbd21e058a522063ddb664cff34c65753f667cbf
Евгений Амплеев <ampleeve@192.168.1.5>
2024-02-28 22:09:04 +0300

    final without linter

--- internal/services/get_user_id_by_number.go
+++ internal/services/get_user_id_by_number.go
@@ -2,7 +2,7 @@ package services
 
 import "context"
 
-func (serv *Services) GetUserIdByOrderNumber(ctx context.Context, ordNumber string) (userID int, err error) {
+func (serv *Services) GetUserIDByOrderNumber(ctx context.Context, ordNumber string) (userID int, err error) {
 	userID, _ = serv.s.GetUserIDByOrder(ctx, ordNumber)
 	return userID, nil
 }
