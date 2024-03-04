52a011dab221fb8a0948f58bee3dbbeebcab86c5
Евгений Амплеев <ampleeve@MacBook-Pro-Evgenij.local>
2024-03-04 08:10:18 +0300

    getorders final

--- internal/services/get_orders_by_user_id.go
+++ internal/services/get_orders_by_user_id.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"fmt"
 	"github.com/eampleev23/diploma/internal/models"
 )
 
@@ -11,5 +12,8 @@ func (serv *Services) GetOrdersByUserID(
 	// Возвращает заказы пользователя по ID
 	serv.l.ZL.Debug("services / GetOrdersByUserID started..")
 	orders, err = serv.s.GetOrdersByUserID(ctx, userID)
+	if err != nil {
+		return orders, fmt.Errorf("store method fail: %w", err)
+	}
 	return orders, nil
 }
