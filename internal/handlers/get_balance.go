508a4972f5b8689010cbfc1eb020bd1a96cafce4
Евгений Амплеев <ampleeve@MacBook-Pro-Evgenij.local>
2024-03-05 17:13:13 +0300

    add checking auth

--- internal/handlers/get_balance.go
+++ internal/handlers/get_balance.go
@@ -1,8 +1,27 @@
 package handlers
 
-import "net/http"
+import (
+	"go.uber.org/zap"
+	"net/http"
+)
 
 // GetBalance возвращает текущую сумму баллов лояльности и сумму использованных баллов
 func (h *Handlers) GetBalance(w http.ResponseWriter, r *http.Request) {
 	h.l.ZL.Debug("Handler GetBalance has started..")
+	// Проверяем авторизацию
+	// Ппроверяем, не авторизован ли пользователь, отправивший запрос.
+	h.l.ZL.Debug("Checking auth..")
+	userID, isAuth, err := h.GetUserID(r)
+	if err != nil {
+		h.l.ZL.Error("GetUserID fail")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if !isAuth {
+		h.l.ZL.Debug("Unauthorized user..")
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+	h.l.ZL.Debug("Authorized user:", zap.Int("userID", userID))
+
 }
