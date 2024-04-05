90f118fb59122abed68643cdbf28d27d8614bb7c
Евгений Амплеев <ampleeve@MacBook-Pro-Evgenij.local>
2024-04-05 17:06:13 +0300

    fix withdraw to float

--- internal/store/db_store_read_func.go
+++ internal/store/db_store_read_func.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"go.uber.org/zap"
+	"log"
 	"strings"
 )
 
@@ -31,6 +32,7 @@ func (d DBStore) GetWithDraw(ctx context.Context, userID int) (withdraw float64,
 	if err != nil {
 		strError := error.Error(err)
 		if strings.Contains(strError, "converting NULL to float64 is unsupported") {
+			log.Println("Here we are! ..")
 			return 0, nil
 		}
 		return 0, fmt.Errorf("QueryRowContext fail: %w", err)
