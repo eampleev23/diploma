fcfae2c073f9bfc6ff73ab6e3342a76d96ad3f00
Евгений Амплеев <ampleeve@192.168.1.3>
2024-04-09 13:18:15 +0300

    fix balance

--- internal/services/get_balance.go
+++ internal/services/get_balance.go
@@ -17,5 +17,6 @@ func (serv *Services) GetBalance(ctx context.Context, userID int) (current, with
 	if err != nil {
 		return 0, 0, fmt.Errorf("GetWithDraw fail: %w", err)
 	}
+	current = current - withdraw
 	return current, withdraw, nil
 }
