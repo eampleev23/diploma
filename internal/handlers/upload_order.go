a7fefd86d70b7adf1b010571caa422b90db81ab7
Евгений Амплеев <ampleeve@MacBook-Pro-Evgenij.local>
2024-04-24 18:33:24 +0300

    back handler without pattern and add question

--- internal/handlers/upload_order.go
+++ internal/handlers/upload_order.go
@@ -2,37 +2,32 @@ package handlers
 
 import (
 	"context"
-	"fmt"
+	"errors"
+	"net/http"
+	"strings"
+
 	"github.com/eampleev23/diploma/internal/models"
 	"github.com/eampleev23/diploma/internal/store"
 	"go.uber.org/zap"
-	"net/http"
-	"strings"
 )
 
 // UploadOrder добавляет новый заказ в систему (заявка на получение баллов лояльности).
 func (h *Handlers) UploadOrder(w http.ResponseWriter, r *http.Request) {
-	if err := h.uploadOrder(r); err != nil {
-		h.l.ZL.Error("failed to handle request to uploadOrder", zap.Error(err))
-		http.Error(w, "internal server error", http.StatusInternalServerError)
-		return
-	}
-}
 
-func (h *Handlers) uploadOrder(r *http.Request) error {
 	// Проверяем формат запроса.
 	contentType := r.Header.Get("Content-Type")
 	textPlain := strings.Contains(contentType, "text/plain")
 	if !textPlain {
-		return fmt.Errorf("format doesn't content text/plain")
+		w.WriteHeader(http.StatusBadRequest)
+		return
 	}
-
 	// Ппроверяем, не авторизован ли пользователь, отправивший запрос.
 	userID, isAuth, err := h.GetUserID(r)
 	if err != nil {
-		return fmt.Errorf("h.GetUserID fail.. %w", err)
+		h.l.ZL.Error("GetUserID fail")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
-
 	if !isAuth {
 		h.l.ZL.Debug("Unaithorized user")
 		w.WriteHeader(http.StatusUnauthorized)
@@ -83,6 +78,9 @@ func (h *Handlers) uploadOrder(r *http.Request) error {
 
 	go h.getFromAccrual(r.Context(), textPlainContent, userID)
 	w.WriteHeader(http.StatusAccepted)
+}
+func uploadOrder(r *http.Request) {
+
 }
 
 func (h *Handlers) getFromAccrual(ctx context.Context, textPlainContent string, userID int) {
